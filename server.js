482a80681623e616ed43da7855ce7a25e8fc5528
MIRANDA <miranda.zermeno4@gmail.com>
2021-10-27 22:53:47 -0500

    revised server.js

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
 // dependencies
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 
 const api = require('./routes/index.js');
 
+// express app for listener 3001
 const PORT = process.env.port || 3001;
 
 const app = express();
 
+// Middleware 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+// GET routes to landing page and note taking page 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
 
 // Wildcard route to direct users to a 404 page
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/pages/404.html'))
 );
 
+
+//Listener
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
