026061a75bbd81c78da8cfe36e82871a59cdd4f4
MIRANDA <miranda.zermeno4@gmail.com>
2021-10-28 20:03:58 -0500

    fixed errs

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
-const router = require("express")
+const express = require("express");
 
+//imports modular routes for 
 const notesRouter = require('./notes');
+
 const app = express();
 
+//use  routes on routes/notes.js 
 app.use('./notes', notesRouter);
 
-module.exports = router;
+module.exports = app;
