13012af60215b1b0ca09a9db71d724be7f9ea594
MIRANDA <miranda.zermeno4@gmail.com>
2021-10-28 19:17:39 -0500

    added POST route to routes/htmlroutes.js

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,14 +1,34 @@
 const notes = require("express").Router();
-const { v4: uuidv4 } = require("uuid");
-const {readFromFile,readAndAppend,writeToFile,} = require("../helpers/fsUtils");
+
 
 // GET Route 
 notes.get("/", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
-// DELETE ROUTE
+// DELETE Route 
 
-// POST 
+
+
+
+// POST route to send data to server to create/update the note taker 
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  // title & text is what is given in db.json
+  const { title, text } = req.body;
+
+  if (req.body) {
+      const newNote = {title,text,id: uuidv4(), // the user will will require tile, text & id to be included in note 
+      };
+
+      readAndAppend(newNote, './db/db.json');
+      res.json("YAY!!✌️ You're note was added "); 
+      // This will display to the user was successful in submitting their note
+  } else {
+      res.error("Oh no! Your note didn't go through 🙁")
+      //This will display to the user if the note was not succesfully entered 
+  };
+});
 
 module.exports = notes;
